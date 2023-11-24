0142e124b150af0201aef9b8d304d5bd4530d77f
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2023-11-24 12:11:53 -0500

    Feed view

---
+++ TikTokClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,105 @@
+//
+//  FeedCell.swift
+//  TikTokClone
+//
+//  Created by ENMANUEL TORRES on 24/11/23.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    let post: Int
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                 .fill(.pink)
+                 .containerRelativeFrame([.horizontal, .vertical])
+                 .overlay {
+                     Text("Post \(post)")
+                         .foregroundStyle(.white)
+                 }
+            VStack{
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("yolima.torres")
+                            .fontWeight(.semibold)
+                            
+                        
+                        Text("rocket prepare for takeoff!!")
+                    }
+                    .foregroundStyle(.white)
+                    .font(.headline)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 28) {
+                        
+                        Circle()
+                            .frame(width: 48, height: 48, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Button{
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                     .resizable()
+                                     .frame(width: 28, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                     .foregroundStyle(.white)
+                                     .bold()
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                        
+                        Button{
+                            
+                        } label: {
+                           
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                     .resizable()
+                                     .frame(width: 28, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                     .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                            
+                        }
+                        
+                        Button{
+                            
+                        } label: {
+                           Image(systemName: "bookmark.fill")
+                                .resizable()
+                                .frame(width: 22, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        Button{
+                            
+                        } label: {
+                           Image(systemName: "arrowshape.turn.up.right.fill")
+                                .resizable()
+                                .frame(width: 28, height: 28, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                    
+                }
+                .padding(.bottom, 80)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    FeedCell(post: 2)
+}
