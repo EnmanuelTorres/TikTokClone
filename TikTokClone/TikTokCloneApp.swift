ad3443311102e5bfc8152237b8964ab5b8db42bc
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2023-11-24 08:57:35 -0500

    Getting started

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by ENMANUEL TORRES on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
